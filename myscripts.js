4fabf7fc419420fb1c758999835ad832e70db270
leohx2 <leobgse@gmail.com>
2022-10-08 21:53:56 +0100

    More changes

--- myscripts.js
+++ myscripts.js
@@ -7,10 +7,12 @@ btnMore.addEventListener('click', function(){
     if (shouldApper == 0){
         menuMore.style.display = "block";
         shouldApper = 1;
+        btnMore.innerText = "More -";
     }
     else if (shouldApper == 1)
     {
         menuMore.style.display = "none";
         shouldApper = 0;
+        btnMore.innerText = "More +";
     }
 });
