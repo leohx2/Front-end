5b485aa05c2aa31fae4a535fabca1faf211adddb
leohx2 <leobgse@gmail.com>
2022-10-09 17:43:21 +0100

    Animation to open and close
    add
    @keyframes and atributtes to animate

--- Exerc 1 - easy/myscripts.js
+++ Exerc 1 - easy/myscripts.js
@@ -5,11 +5,18 @@ const menuMore = document.getElementById("menuMoreId");
 btnMore.addEventListener('click', function(){
     if (!menuMore.hasAttribute('open')){
         menuMore.setAttribute('open', "");
+        if(menuMore.hasAttribute('closed'))
+        {
+            menuMore.removeAttribute('closed');
+        }
         btnMore.innerText = "More -";
     }
     else if (menuMore.hasAttribute('open'))
     {
-        menuMore.removeAttribute('open');
-        btnMore.innerText = "More +";
+        menuMore.setAttribute('closed', "");
+        menuMore.addEventListener('animationend', function(){
+            menuMore.removeAttribute('open');
+            btnMore.innerText = "More +";
+        }, {once: true})
     }
 });
