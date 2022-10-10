126339b57a5cab8e2d8f598ce4bb1c18b833539f
leohx2 <leobgse@gmail.com>
2022-10-10 19:45:01 +0100

    First try navbar - not working

--- Exerc 1 - easy/myscripts.js
+++ Exerc 1 - easy/myscripts.js
@@ -1,6 +1,8 @@
 
 const btnMore = document.getElementById("btnMoreId");
 const menuMore = document.getElementById("menuMoreId");
+const navMenu = document.querySelector(".menuScrollFlex");
+let lastScrollY = window.scrollY;
 
 btnMore.addEventListener('click', function(){
     if (!menuMore.hasAttribute('open')){
@@ -18,5 +20,22 @@ btnMore.addEventListener('click', function(){
             menuMore.removeAttribute('open');
             btnMore.innerText = "More +";
         }, {once: true})
+    }
+});
+
+window.addEventListener("scroll", function(){
+    if (lastScrollY > this.window.scrollY){
+        console.log("scroll Down");
+        lastScrollY = window.scrollY;
+        if (navMenu.hasAttribute("up"))
+        {
+            navMenu.removeAttribute("up");
+        }
+    }
+    else {
+        console.log("scroll Up");
+        lastScrollY = window.scrollY;
+        navMenu.setAttribute('up', "");
+        console.log(window.scrollY);
     }
 });
